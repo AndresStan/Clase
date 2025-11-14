8eb595b6043764bd193fc88c253dca198853748b
Andres Stan <aastan@iesch.org>
2025-11-14 17:56:40 +0100

    clase

---
+++ Programacion/UD3/ejercicios4 tema3/src/ejercicio32.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ejercicio32 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Porfavor, indica un numero entero");
+        int num = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = num; i >= 1; i--) {
+
+
+
+        }
+
+
+
+    }
+
+
+
+}
