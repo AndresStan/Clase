e3327e5d324d1006c4b9e727d52f9384767360a8
Andres Stan <aastan@iesch.org>
2026-02-12 19:35:45 +0100

    AutoPush

--- Programacion/UD11/Ejercicios 1/src/Ejercicio3/Main.java
+++ Programacion/UD11/Ejercicios 1/src/Ejercicio3/Main.java
@@ -1,19 +1,32 @@
 package Ejercicio3;
 
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+        Random random = new Random();
 
+        List<Integer> lista = new ArrayList<>();
+        int numero;
 
         for (int i = 1; i <= 50; i++) {
-
-
-
+        lista.add(random.nextInt(0,100));
         }
 
+        try (Scanner sc = new Scanner(System.in)){
+            System.out.println("Porfavor indica un numero");
+            numero = sc.nextInt();
+        } catch (Exception e) {
+            System.out.println("Ha habido un error");
+            throw new RuntimeException(e);
+        }
 
+        long contador = lista.stream().filter(integer -> integer == numero).count();
+        System.out.println("Hay " + contador + " en la lista");
     }
 }
