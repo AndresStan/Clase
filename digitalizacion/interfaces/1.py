dd1645e28a6b6d2b9de0c725cc5a029cb34ce842
Andres Stan <aastan@iesch.org>
2026-03-04 18:24:54 +0100

    AutoPush

--- digitalizacion/interfaces/1.py
+++ digitalizacion/interfaces/1.py
@@ -1,37 +1,38 @@
 import tkinter as tk
-#from tkinter import ttk, Widget
+from tkinter import ttk, Widget
 
 root = tk.Tk()
 root.title("Hola")
 root.config(width=400, height=400)
 label = tk.Label(text="Formulario Datos")
 
-labelNombre = tk.Label(text="Nombre")
-entryNombre = tk.Entry(width=40, bg="white", justify="center")
+frame1 = tk.Frame(bg="red", width=200, height=200)
+frame1.grid(row=0, column=0, padx=20,pady=20)
+label1 = tk.Label(frame1, text="Nivel de estudios")
+label1.place(x=50, y=50)
+radiobutton1= ttk.Radiobutton(frame1, text="Estudios basicos")
+radiobutton1.place(x=50, y=80)
 
-labelApellidos = tk.Label(text="Apellidos")
-entryApellidos = tk.Entry(width=40, bg="white", justify="center")
+radiobutton2= ttk.Radiobutton(frame1, text="Estudios medios")
+radiobutton2.place(x=50, y=110)
 
-labelEdad = tk.Label(text="Edad")
-entryEdad = tk.Entry(width=40, bg="white", justify="center")
+radiobutton3= ttk.Radiobutton(frame1, text="Estudios avanzados")
+radiobutton3.place(x=50, y=140)
 
-labelTelefono = tk.Label(text="Teléfono")
-entryTelefono = tk.Entry(width=40, bg="white", justify="center")
 
-label.grid(row=0, column=0, columnspan=2)
 
-labelNombre.grid(row=1, column=0, padx=8, pady=10)
-entryNombre.grid(row=1, column=1, padx=8, pady=10)
+frame2 = tk.Frame(bg="blue", width=200, height=200)
+frame2.grid(row=0, column=1,padx=20,pady=20)
+label2 = tk.Label(frame2, text="Aficiones")
+label2.place(x=50, y=50)
 
-labelApellidos.grid(row=2, column=0, padx=8, pady=10)
-entryApellidos.grid(row=2, column=1, padx=8, pady=10)
 
-labelEdad.grid(row=3, column=0, padx=8, pady=10)
-entryEdad.grid(row=3, column=1, padx=8, pady=10)
+frame3 = tk.Frame(bg="yellow", width=200, height=200)
+frame3.grid(row=1, column=0,padx=20,pady=20)
 
-labelTelefono.grid(row=4, column=0, padx=8, pady=10)
-entryTelefono.grid(row=4, column=1, padx=8, pady=10)
 
+frame4 = tk.Frame(bg="green", width=200, height=200)
+frame4.grid(row=1, column=1,padx=20,pady=20)
 
 
 root.mainloop()
