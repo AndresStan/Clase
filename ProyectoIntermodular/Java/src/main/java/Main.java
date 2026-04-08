db5292dfe0c2d55b129f604f7505feba7406171c
Andres Stan <aastan@iesch.org>
2026-04-08 17:41:55 +0200

    AutoPush

---
+++ ProyectoIntermodular/Java/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import Modelo.Conexion;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println(Conexion.crearConexion());
+
+
+
+    }
+}
--- ProyectoIntermodular/Java/src/main/java/Modelo/Conexion.java
+++ ProyectoIntermodular/Java/src/main/java/Modelo/Conexion.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conexion {
 
     public static Connection crearConexion() throws SQLException {
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/tienda_libros", "root", "1234");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/gimnasiointermodular", "root", "1234");
         System.out.println("Se ha conectado exitosamente a la base de datos");
         return connection;
     }
