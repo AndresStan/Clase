be5bb255f689de6ac10d7846e6dca23d006a2853
Andres Stan <aastan@iesch.org>
2026-05-07 19:53:36 +0200

    AutoPush

--- ProyectoIntermodular/Java/src/main/java/Controlador/Conexion.java
+++ ProyectoIntermodular/Java/src/main/java/Controlador/Conexion.java
@@ -1,19 +1,28 @@
 package Controlador;
 
+import java.io.*;
+import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Conexion {
 
     public static Connection crearConexion() throws SQLException {
 
-        String url;
-        String user;
-        String password;
-
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/gimnasiointermodular", "root", "1234");
+        Path ruta = Path.of("src/main/java/Controlador/DatosConexion.txt");
+        File archivo = ruta.toFile();
+        ArrayList<String> lista = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(archivo))) {
+            String line;
+            while ((line = reader.readLine()) != null){
+                lista.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Connection connection = DriverManager.getConnection(lista.get(0), lista.get(1), lista.get(2));
         System.out.println("Se ha conectado exitosamente a la base de datos");
         return connection;
     }
