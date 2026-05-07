8fd32076078548855e054b83f95d2d8946c9588e
Andres Stan <aastan@iesch.org>
2026-05-07 17:36:12 +0200

    AutoPush

---
+++ LenguajeDeMarcas/3EV/UD8(VUE,ETC...)/componentes/src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue';
+import BlogPost from './componentes/BlogPost.vue';
+import BotonContador from './componentes/BotonContador.vue';
+
+const posts = ref([
+  {id: 1, title:"Post 01", contenido:"Contenido del post 01"},
+  {id: 2, title:"Post 02", contenido:"Contenido del post 02"},
+  {id: 3, title:"Post 03", contenido:"Contenido del post 03"},
+  {id: 4, title:"Post 03", contenido:"Contenido del post 04"},
+  {id: 5, title:"Post 05"}
+])
+
+</script>
+
+<template>
+  <h1>DAM 1 - 2026</h1>
+  <BotonContador></BotonContador>
+  <hr>
+  <div class="container">
+      <BlogPost v-for="post in posts" :key="post.id"
+      :title="post.title" :contenido="post.contenido" :id="post.id"/>
+  </div>
+ 
+</template>
+
+<style scoped></style>
---
+++ LenguajeDeMarcas/3EV/UD8(VUE,ETC...)/componentes/src/componentes/BlogPost.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+    // defineProps(['title', 'contenido', 'id'])
+    const props = defineProps({
+        title: String,
+        id: Number,
+        contenido: {
+            type: String,
+            default: 'Sin descripción'
+        }
+    })
+
+</script>
+<template>
+    <div class="card">
+        <div class="card-body">
+            <h4 class="card-title">
+                {{ title }}
+            </h4>
+            <p>
+              {{ id }} {{contenido}} 
+            </p>
+        </div>
+    </div>
+</template>
